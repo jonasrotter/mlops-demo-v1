2d0f265e1c3a47a24cb004a094404bc95983e163
Jonas Rotter <jonasrotter@microsoft.com>
2022-05-17 21:46:42 +0000

    Updated training.py

--- models/german-credit-basic/pipelines/training.py
+++ models/german-credit-basic/pipelines/training.py
@@ -17,11 +17,10 @@ ws = Workspace.from_config()
 print(f'WS name: {ws.name}\nRegion: {ws.location}\nSubscription id: {ws.subscription_id}\nResource group: {ws.resource_group}')
 
 print('Loading dataset')
-training_dataset = Dataset.get_by_name(ws, name='german_credit_file')
 
 # Parametrize dataset input to the pipeline
-#training_dataset_parameter = PipelineParameter(name="training_dataset", default_value=training_dataset)
-#training_dataset_consumption = DatasetConsumptionConfig("training_dataset", training_dataset_parameter).as_download()
+training_dataset_parameter = PipelineParameter(name="training_dataset", default_value=training_dataset)
+training_dataset_consumption = DatasetConsumptionConfig("training_dataset", training_dataset_parameter).as_download()
 
 runconfig = RunConfiguration()
 runconfig.environment = Environment.from_conda_specification('training-pipeline-env', 'config/train-conda.yml')
