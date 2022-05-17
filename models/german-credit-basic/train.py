30b41950bcf5867e1d0f05090d467c70cce32c50
Jonas Rotter <jonasrotter@microsoft.com>
2022-05-17 20:12:22 +0000

    Updated train.py

--- models/german-credit-basic/train.py
+++ models/german-credit-basic/train.py
@@ -15,7 +15,7 @@ from azureml.interpret import ExplanationClient
 
 def get_runtime_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=str)
+    parser.add_argument('--model_name')
     parser.add_argument('--data_path', type=str)
     args = parser.parse_args()
     return args
